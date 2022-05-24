902e3db35c795291496d1fdc632bd66ebcb51841
Giannina <giannisedler@hotmail.com>
2022-05-24 15:27:48 +0800

    hmw 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,32 +2,54 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  var sum = 0;
-  var numReverse = num.split("").reverse().join("");
+  
+  var suma = 0;
 
-  for (let i = 0; i<numReverse.length; i++){
-    sum = sum + numReverse[i] * 2 **i;
+  // num = num.split("").reverse().join("")    //O doy vuelta el numero antes y despues lo potencio a la posicion (i)
 
-  }
-  return sum;
+  // for (let i = 0; i<num.length; i++){
+  // suma += num[i] * (2**i);
+  // }
+ 
+  for (let i = 0; i<num.length; i++){
+    suma += num[i] * 2 **(num.length -1 -i);   // O lo tengo que potenciar a la (num.length -1 -i) 
+  }                                            // para que la pocision sea de der a izq
+
+  return suma;
+
+  // return parseInt(num, 2); // esto reemplaza a todo el codigo anterior 
 
 }
 
 
 function DecimalABinario(num) {
   // tu codigo aca
-  let numBinario = [];
-  while (num !== 0){
-    let residuo = (num%2);
-    num = Math.floor(num/2);
-    numBinario = numBinario + residuo.toString()
+  var numBinario = "";
+
+  while (num > 0){
+    numBinario = num % 2 + numBinario // Esto es para que el resto de num/2 se agregue adelante del numero existente y no al final 
+    num = Math.floor(num/2);          // Tambien podria declarar la var numBinario como un array y hacer unshift de (num % 2) 
+                                      // Despues haria numBinario.join("") para pasar array a string
 
     }
-
     
-  return numBinario.split("").reverse().join("");
+    return numBinario;
+
+  // return numBinario.split("").reverse().join(""); // si haria (numBinario += num % 2) en vez de (numBinario = num % 2 + numBinario)
+                                                     // podria hacer esto para dar vuelta el numero final
+                                                     
+  // return num.toString(2); // esto reemplaza a todo el codigo anterior 
+
 
 }
+
+
+// 14 / 2     7 | 0
+//  7 / 2     3 | 1
+//  3 / 2     1 | 1
+//  1 / 2     0 | 1   
+//  
+// "14" -->  "1110"
 
 
 module.exports = {
